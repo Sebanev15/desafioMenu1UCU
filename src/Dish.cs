ffeef5dcfc3267f64c3f4adbadefef68ce61da80
Agustín Montesdeoca <agustinmontesdeoca38@gmail.com>
2025-09-01 15:19:27 -0300

    Realización de la clase Dish y su estructura inicial

--- src/Dish.cs
+++ src/Dish.cs
@@ -1,10 +1,16 @@
 namespace Ucu.Poo.Restaurant
 {
-
-    /// <summary>
-    /// Representa un platillo individual en el menú del restaurante.
-    /// </summary>
     public class Dish
     {
+        private string Name { get; set; }
+        private float Price { get; set; }
+        private bool IsVegetarian { get; set; }
+
+        public Dish( string name, float price, bool isVegetarian )
+        {
+            this.Name = name;
+            this.Price = price;
+            this.IsVegetarian = isVegetarian;
+        }
     }
 }
